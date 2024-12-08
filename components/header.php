9542d7719a9e834397f0f3e2290399d3f4503922
bogdan <brasskazov@gmail.com>
2024-12-08 18:41:09 +0300

    feat: nav

--- components/header.php
+++ components/header.php
@@ -6,10 +6,21 @@
 ?>
 
 <header class="header">
-    <h2 class="profile-name">  <?php echo $user['name'] ?> </h2>
-    <img class="avatar" src = "<?php echo $user['avatar'] ?>"  alt="<?php echo $user['name'] ?>" >
+    <nav>
+        <ul>
+            <li>
+                <a href="/home.php">
+                    Home
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <div class="header__user">
+        <h2 class="profile-name">  <?php echo $user['name'] ?> </h2>
+        <img class="avatar" src = "<?php echo $user['avatar'] ?>"  alt="<?php echo $user['name'] ?>" >
 
-    <form class="logout" method="post" action="/src/actions/logout.php">
-        <button role="button">Выйти</button>
-    </form>
+        <form class="logout" method="post" action="/src/actions/logout.php">
+            <button role="button">Выйти</button>
+        </form>
+    </div>
 </header>
